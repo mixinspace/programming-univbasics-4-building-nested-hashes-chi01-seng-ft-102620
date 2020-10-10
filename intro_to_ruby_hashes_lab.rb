6ebf467f876b1216a5b9925530ebe47ea8875884
Islam Madkaur <islammadkaur@gmail.com>
2020-10-10 19:46:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,6 +9,8 @@ end
 
 def monopoly_with_second_tier
   {
+    :railroads => {
+  
     {
       :pieces => 4
     }
@@ -19,6 +21,7 @@ def monopoly_with_second_tier
       :names => {}
     }
   }
+}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
